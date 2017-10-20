5ce5aeb1ff492f4b71533e62b171d5938306dc24
JiangBao <873575504@qq.com>
2017-10-20 15:20:23 +0800

    add problem single-number

---
+++ single-number/index.js
@@ -0,0 +1,23 @@
+/**
+ * @param  {number[]} nums
+ * @return {number}
+ */
+const singleNumber = (nums) => {
+  let res = 0, len = nums.length;
+  for (let i = 0; i < len; i++) {
+    res ^= nums[i];
+  }
+
+  return res;
+
+  // let map = {}, res;
+  // for (let i = 0; i < nums.length; i++) {
+  //   map[nums[i]] = map[nums[i]] ? map[nums[i]] + 1 : 1;
+  // }
+  // for (let i in map) {
+  //   if (map[i] === 1) return parseInt(i);
+  // }
+}
+
+const res = singleNumber([2, 6, 1, 2, 6]);
+console.log('===> ' + res);
