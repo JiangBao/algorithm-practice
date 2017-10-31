50074f792ed8f9e139944a582a7f48ef037e9b03
JiangBao <873575504@qq.com>
2017-10-31 15:50:30 +0800

    add problem majority-element

---
+++ majority-element/README.md
@@ -0,0 +1,5 @@
+# maority element
+## 问题
+给出一个长度为`n`的数组，找出其中出现次数大于`n/2` 的元素
+
+[传送门](https://leetcode.com/problems/majority-element/description/)
---
+++ majority-element/index.js
@@ -0,0 +1,21 @@
+/**
+ * @param  {number[]} nums
+ * @return {number}
+ */
+const majorityElement = (nums) => {
+  let major = nums[0], count = 1;
+  for (let i = 1, len = nums.length; i < len; i++) {
+    if (count === 0) {
+      count++;
+      major = nums[i];
+    } else {
+      if (major === nums[i]) {
+        count++;
+      } else {
+        count--;
+      }
+    }
+  }
+
+  return major;
+}
