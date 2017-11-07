9600e46d445be893d8531f98c89004caa533ec01
JiangBao <873575504@qq.com>
2017-11-07 12:03:00 +0800

    add problem contains-duplicate

---
+++ contains-duplicate/README.md
@@ -0,0 +1,13 @@
+# contains duplicate
+# 问题
+查找一个整型数组中是否有重复元素  
+**example:**
+```
+Input: [1, 2, 3, 4, 5]
+Output: false
+
+Input: [1, 2, 3, 2, 5]
+Output: true
+```
+
+[传送门](https://leetcode.com/problems/contains-duplicate/description/)
---
+++ contains-duplicate/index.js
@@ -0,0 +1,20 @@
+/**
+ * @param  {number[]} nums
+ * @return {boolean}
+ */
+const containsDuplicate = (nums) => {
+  let map = {}, len = nums.length;
+  if (len <= 1) {
+    return false;
+  }
+  for (let i = 0; i < len; i++) {
+    if (map.hasOwnProperty(nums[i])) {
+      return true;
+    } else {
+      map[nums[i]] = 0;
+    }
+  }
+  return false; 
+}
+
+console.log(containsDuplicate([3, 3]));
