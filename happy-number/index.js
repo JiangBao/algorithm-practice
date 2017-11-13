b3908e8e3de753fdda3ee137b4711f8f8460e7d0
JiangBao <873575504@qq.com>
2017-11-13 14:03:34 +0800

    add solution

--- happy-number/index.js
+++ happy-number/index.js
@@ -3,5 +3,31 @@
  * @return {boolean}
  */
 const isHappy = (n) => {
-  
+  let x = n, y = n;
+
+  while (x > 1) {
+    x = cal(x);
+    if (x === 1) {return true;}
+    y = cal(cal(y));
+    if (y === 1) {return true;}
+    if (x === y) {return false;}
+  }
+
+  return true;
+}
+
+/**
+ * sum of the squares of its digits
+ * @param  {number} n
+ * @return {number}
+ */
+function cal(n) {
+  let x = n, s = 0;
+
+  while (x > 0) {
+    s = s + (x % 10) * (x % 10);
+    x = Math.floor(x / 10);
+  }
+
+  return s;
 }
